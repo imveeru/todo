350f1b7558aea172dd6a291bbfeb4a5185da4ea7
imvee <vind.veeru@gmail.com>
2021-06-12 20:43:49 +0530

    :sparkles: Added Checkbox feature.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import './TodoItem.css'
+import {useDispatch} from 'react-redux'
+import {setCheck} from '../features/todoSlice'
 
 import Checkbox from '@material-ui/core/Checkbox'
 
-const handleCheck=()=>{
-
-}
-
 function TodoItem({name,done,id}) {
+
+    const dispatch=useDispatch()
+
+    const handleCheck=()=>{
+        dispatch(setCheck(id))
+    }
+
     return (
         <div className="todoItem">
             <Checkbox
